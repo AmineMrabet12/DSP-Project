2b8262b4413cdb9cef5f6505bbd05c4413fee93f
AmineMrabet12 <mrabetmohamedamine1@gmail.com>
2024-10-02 16:00:11 +0200

    Updating missing functions

--- code/app/database.py
+++ code/app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, MetaData
 from databases import Database
 
-# DATABASE_URL = "postgresql://mohamedaminemrabet:amine@localhost:5432/epita"
-DATABASE_URL = "postgresql://wasedoo:postgres@localhost:5432/epita"
+DATABASE_URL = "postgresql://mohamedaminemrabet:amine@localhost:5432/epita"
+# DATABASE_URL = "postgresql://wasedoo:postgres@localhost:5432/epita"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
